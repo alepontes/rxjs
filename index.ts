e769baa82bd532132dbb241190c03f08cfd6caae
alepontes <alexandrelopespontes@gmail.com>
2022-10-10 23:37:05 -0300

    add calc

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-import { interval } from 'rxjs';
+import { combineLatest, interval, map } from 'rxjs';
 
-interval(1000).subscribe(_ => console.log(_, `Hello World!`))
+/**
+ * npm start
+ * Observa A e B e refaz a conta quando os valores mudam
+ */
+
+const a$ = interval(2000);
+const b$ = interval(3000);
+
+const r$ = combineLatest([a$, b$]).pipe(
+    map(([a, b]) => ([a, b, a + b]))
+);
+
+r$.subscribe(([a, b, r]) => console.log(`${a} + ${b} = ${r}`));
